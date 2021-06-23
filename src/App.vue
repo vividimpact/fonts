a446798d9ff0fbb48819c875b6d0f10cf9def812
Michael Johnson <michael.johnson@vividimpact.com>
2021-06-23 13:35:00 -0400

    component revision

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/img/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Home msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
